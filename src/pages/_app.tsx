c4fb4f309dd6242732d74f3fc4c7b0b527177e26
Erik Skaar <erik.skaar@kantega.no>
2024-02-05 16:55:19 +0100

    add theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {" "}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
