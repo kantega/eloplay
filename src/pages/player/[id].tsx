1edc2d7311ae1bffdd03ebd25b896a81d31e7922
eSkaarAS <122200628+eSkaarAS@users.noreply.github.com>
2024-02-07 20:16:48 +0100

    Implement player elo graph and opponent radar graph (#3)
    * implement player elo graph
    
    * implement opponents radar
    
    * implement player elo graph and opponents radar
    
    ---------
    
    Co-authored-by: Erik Skaar <erik.skaar@kantega.no>

--- src/pages/player/[id].tsx
+++ src/pages/player/[id].tsx
@@ -3,6 +3,8 @@ import MatchHistory from "./MatchHistory";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import PlayerCard from "./PlayerCard";
+import PlayerEloGraph from "./PlayerEloGraph";
+import PlayerOpponentRadar from "./PlayerOpponentRadar";
 
 export default function PlayerPage() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -13,6 +15,12 @@ export default function PlayerPage() {
     <div className="container flex h-full flex-col items-center gap-8 px-4 py-4 ">
       {typeof id === "string" && (
         <PlayerCard id={id} searchQuery={searchQuery} />
+      )}
+      {typeof id === "string" && (
+        <PlayerEloGraph id={id} searchQuery={searchQuery} />
+      )}
+      {typeof id === "string" && (
+        <PlayerOpponentRadar id={id} searchQuery={searchQuery} />
       )}
       <Input
         placeholder="search for opponent..."
