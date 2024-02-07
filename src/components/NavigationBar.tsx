9bdb49187a0bfd06ace65e8114ce4162c8c7c1bc
Erik Skaar <erik.skaar@kantega.no>
2024-02-07 09:03:07 +0100

    implement better navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 export default function NavigationBar() {
   return (
     <div className="fixed bottom-0 right-0 flex w-full justify-center bg-background">
-      <div className=" flex w-[min(500px,100%)]  justify-around bg-background py-6">
-        <Link href={"/"}>
+      <div className=" flex w-[min(500px,100%)]  justify-around bg-background">
+        <Link href={"/"} className="p-4">
           <Medal />
         </Link>
-        <Link href={"/addMatch"}>
+        <Link href={"/addMatch"} className="p-4">
           <PlusCircle />
         </Link>
-        <Link href={"/addPlayer"}>
+        <Link href={"/addPlayer"} className="p-4">
           <UserPlus />
         </Link>
       </div>
