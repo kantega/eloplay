cb2fecfdc322ceceb39c431d6f9a4900eae07680
eSkaarAS <122200628+eSkaarAS@users.noreply.github.com>
2024-02-08 09:27:25 +0100

    bugfix for search, and match history and elo graph order (#8)
    problem:
    match history showed oldest match first
    filterMatches function call in playercard is missing id arg
    elo graph shows reversed elo graph
    
    Co-authored-by: Erik Skaar <erik.skaar@kantega.no>

--- src/pages/player/MatchHistory.tsx
+++ src/pages/player/MatchHistory.tsx
@@ -26,58 +26,56 @@ export default function MatchHistory({
   const filteredMatches = filterMatches(data, searchQuery, id);
 
   return (
-    <>
-      <Table className="w-[min(500px,100%)]">
-        <TableHeader>
-          <TableRow>
-            <TableHead>Vinner</TableHead>
-            <TableHead>Taper</TableHead>
-            <TableHead>
-              <Plus color="limegreen" />
-              <Minus color="red" />
-            </TableHead>
-            <TableHead>
-              <InfoIcon />
-            </TableHead>
+    <Table className="w-[min(500px,100%)]">
+      <TableHeader>
+        <TableRow>
+          <TableHead>Vinner</TableHead>
+          <TableHead>Taper</TableHead>
+          <TableHead>
+            <Plus color="limegreen" />
+            <Minus color="red" />
+          </TableHead>
+          <TableHead>
+            <InfoIcon />
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {filteredMatches.map((match) => (
+          <TableRow key={match.id}>
+            <TableCell className="font-medium">
+              {`${match.player1Id} (${match.prePlayer1Elo})`}
+            </TableCell>
+            <TableCell className="font-medium">
+              {`${match.player2Id} (${match.prePlayer2Elo})`}
+            </TableCell>
+            <TableCell
+              className={
+                id === match.winner ? "text-green-500" : "text-red-500"
+              }
+            >
+              {id === match.winner
+                ? "+" +
+                  (updateEloRating(
+                    match.prePlayer1Elo,
+                    match.prePlayer2Elo,
+                    matchResults.player111,
+                  )[0] -
+                    match.prePlayer1Elo)
+                : updateEloRating(
+                    match.prePlayer1Elo,
+                    match.prePlayer2Elo,
+                    matchResults.player111,
+                  )[1] - match.prePlayer2Elo}
+            </TableCell>
+            <TableCell>
+              <MatchDialog match={match} id={id}>
+                <Info />
+              </MatchDialog>
+            </TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {filteredMatches.map((match) => (
-            <TableRow key={match.id}>
-              <TableCell className="font-medium">
-                {`${match.player1Id} (${match.prePlayer1Elo})`}
-              </TableCell>
-              <TableCell className="font-medium">
-                {`${match.player2Id} (${match.prePlayer2Elo})`}
-              </TableCell>
-              <TableCell
-                className={
-                  id === match.winner ? "text-green-500" : "text-red-500"
-                }
-              >
-                {id === match.winner
-                  ? "+" +
-                    (updateEloRating(
-                      match.prePlayer1Elo,
-                      match.prePlayer2Elo,
-                      matchResults.player111,
-                    )[0] -
-                      match.prePlayer1Elo)
-                  : updateEloRating(
-                      match.prePlayer1Elo,
-                      match.prePlayer2Elo,
-                      matchResults.player111,
-                    )[1] - match.prePlayer2Elo}
-              </TableCell>
-              <TableCell>
-                <MatchDialog match={match} id={id}>
-                  <Info />
-                </MatchDialog>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
