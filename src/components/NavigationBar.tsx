39d43bd266ec4b5d8fe73762843575a01e2dd3bd
Erik Skaar <erik.skaar@kantega.no>
2024-02-14 10:08:41 +0100

    navbar and settingsbar to reflect upcoming changes

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,7 +5,7 @@ export default function NavigationBar() {
   return (
     <div className="fixed bottom-0 right-0 flex w-full justify-center bg-background">
       <div className=" flex w-[min(500px,100%)]  justify-around bg-background">
-        <Link href={"/"} className="p-4">
+        <Link href={"/leaderboard"} className="p-4">
           <Medal />
         </Link>
         <Link href={"/addMatch"} className="p-4">
--- src/components/SettingsBar.tsx
+++ src/components/SettingsBar.tsx
@@ -1,7 +1,6 @@
 import { ModeToggle } from "@/contexts/themeContext/theme-toggle";
 import { LocationSelector } from "@/contexts/locationContext/location-toggle";
 import { signIn, signOut, useSession } from "next-auth/react";
-// import { api } from "@/utils/api";
 import { Button } from "@/components/ui/button";
 
 export default function NavigationBar() {
@@ -11,32 +10,21 @@ export default function NavigationBar() {
 
       <span className="flex gap-4">
         <ModeToggle />
-        <AuthShowcase />
+        <SignInOrOut />
       </span>
     </div>
   );
 }
 
-function AuthShowcase() {
+function SignInOrOut() {
   const { data: sessionData } = useSession();
 
-  // const { data: secretMessage } = api.player.getSecretMessage.useQuery(
-  //   undefined, // no input
-  //   { enabled: sessionData?.user !== undefined },
-  // );
-
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      {/* <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p> */}
-      <Button
-        variant={sessionData ? "outline" : "default"}
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </Button>
-    </div>
+    <Button
+      variant={sessionData ? "outline" : "default"}
+      onClick={sessionData ? () => void signOut() : () => void signIn()}
+    >
+      {sessionData ? "Sign out" : "Sign in"}
+    </Button>
   );
 }
