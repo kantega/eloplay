2b521fd32cd5baf3faaa3ea8bd6935428123f9a3
Erik Skaar <erik.skaar@kantega.no>
2024-02-23 13:02:24 +0100

    implement useCheckForTeam hook and redirect user if user got any teams

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ import { useContext } from "react";
 import { TeamContext } from "@/contexts/teamContext/team-provider";
 import { useRouter } from "next/router";
 import { api } from "@/utils/api";
-import { Button } from "@/components/ui/button";
 
 export default function CreateOrJoinO() {
   const { data: sessionData } = useSession();
+  useCheckForTeam();
 
   if (!sessionData)
     return (
@@ -22,45 +22,21 @@ export default function CreateOrJoinO() {
     <div className="container flex h-full w-full flex-col items-start gap-12 px-4 py-4 ">
       <HeaderLabel headerText={"Join or create team"} label="ELOPLAY" />
       <JoinTeamForm />
-      <GoToTeamFromTeamList />
       <CreateTeamForm />
     </div>
   );
 }
 
-function GoToTeamFromTeamList() {
+function useCheckForTeam() {
+  const { setTeamId } = useContext(TeamContext);
+  const { data, isLoading } = api.team.getAll.useQuery();
   const { teamId } = useContext(TeamContext);
   const router = useRouter();
 
   if (teamId !== "") void router.push("/leaderboard");
 
-  return <TeamList />;
-}
-
-function TeamList() {
-  const { setTeamId } = useContext(TeamContext);
-  const { data, isLoading } = api.team.getAll.useQuery();
-
   if (isLoading || !data) return null;
   if (data.length === 0) return null;
-
-  return (
-    <div className="w-full space-y-4">
-      <h1 className="m-0 text-2xl">Pick a team</h1>
-      <ul className="w-full space-y-2">
-        {data.map((team) => (
-          <li key={team.id} className="w-full">
-            <Button
-              className="w-full text-xl text-background"
-              onClick={() => {
-                setTeamId(team.id);
-              }}
-            >
-              {team.name}
-            </Button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (!!data[0]) setTeamId(data[0].id);
+  return null;
 }
