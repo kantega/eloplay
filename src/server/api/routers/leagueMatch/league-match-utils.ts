b92d0233706989c6c69c5567b6aa7b675da104d3
Erik Skaar <erik.skaar@kantega.no>
2024-02-23 08:37:50 +0100

    remove unused code

--- src/server/api/routers/leagueMatch/league-match-utils.ts
+++ src/server/api/routers/leagueMatch/league-match-utils.ts
@@ -1,57 +1,6 @@
 import { latestEloGainSchema } from "@/server/api/routers/leagueMatch/league-match-types";
 import { type RoleText } from "@/server/types/roleTypes";
-import { type TableTennisMatch, type TeamUser } from "@prisma/client";
-
-export const sortMatchesByDate = (a: TableTennisMatch, b: TableTennisMatch) => {
-  return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-};
-
-export const getMatchStats = (matches: TableTennisMatch[], id: string) => {
-  const winrate = Math.ceil(
-    (matches.filter((match) => match.winner === id).length / matches.length) *
-      100,
-  );
-  let isWinstreaking = true;
-  const winstreak = matches.reduce((acc, match) => {
-    if (match.winner === id && isWinstreaking) {
-      return acc + 1;
-    } else {
-      isWinstreaking = false;
-      return acc;
-    }
-  }, 0);
-  return { winrate, winstreak };
-};
-
-export function filterMatches(
-  matches: TableTennisMatch[],
-  searchQuery: string,
-  id: string,
-) {
-  const letters = searchQuery.split("");
-
-  const filteredMatches = matches.filter((match) => {
-    return letters.reduce(
-      (acc, letter) => {
-        if (!acc.state) return { state: acc.state, name: acc.name };
-        const includesLetter = acc.name.includes(letter.toLowerCase());
-        return {
-          state: includesLetter,
-          name: acc.name.replace(letter.toLowerCase(), ""),
-        };
-      },
-      {
-        state: true,
-        name:
-          match.winner === id
-            ? match.player2Id.toLowerCase()
-            : match.player1Id.toLowerCase(),
-      },
-    ).state;
-  });
-
-  return filteredMatches.reverse();
-}
+import { type TeamUser } from "@prisma/client";
 
 export interface TeamMemberProps extends TeamUser {
   role: RoleText;
