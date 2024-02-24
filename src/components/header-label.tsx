368afe01672a3d0fd77160534306044b6bc356fd
Erik Skaar <erik.skaar@kantega.no>
2024-02-24 10:26:12 +0100

    implement font Inter to application

--- src/components/header-label.tsx
+++ src/components/header-label.tsx
@@ -10,7 +10,7 @@ export default function HeaderLabel({
   return (
     <span>
       {label && (
-        <h2 className=" m-0 text-xs font-bold text-gray-500">{label}</h2>
+        <h2 className="m-0 text-xs font-bold text-gray-500">{label}</h2>
       )}
       <h1 className="m-0 text-4xl text-primary">{headerText}</h1>
     </span>
