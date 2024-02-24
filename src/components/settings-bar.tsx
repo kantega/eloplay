ee552b8125d5a9179dc114f13774756c75a8c411
Erik Skaar <erik.skaar@kantega.no>
2024-02-24 18:40:48 +0100

    bugfix: visual bug where profile picker was on left side

--- src/components/settings-bar.tsx
+++ src/components/settings-bar.tsx
@@ -5,7 +5,9 @@ import { LeagueSelector } from "@/contexts/leagueContext/league-selector";
 export default function NavigationBar() {
   return (
     <div className="sticky top-0 z-50 flex w-full flex-row justify-between bg-background px-1 py-2">
-      <LeagueSelector />
+      <span>
+        <LeagueSelector />
+      </span>
 
       <span className="flex gap-4">
         <ModeToggle />
